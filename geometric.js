f4f86dec2da27ba47d41931545aaf656bef1ec33
lermantorres <108762074+lermantorres@users.noreply.github.com>
2024-05-30 18:22:28 -0500

    agrego funciones
    agrego funciones

---
+++ geometric.js
@@ -0,0 +1,12 @@
+//defino funciones 
+
+let area_cuadrado = (lado)=> lado * lado;
+let perimetro_cuadrado = (lado)=> 4 * lado;
+
+let area_triangulo = (base,altura)=> (base*altura)/2;
+let perimetro_triangulo = (lado1,lado2,lado3)=> lado1+lado2+lado3;
+
+let area_circulo = (radio)=> (radio**2)*3.14;
+let perimetro_circulo = (radio)=> 2*3.14*radio;
+let diametro_circulo = (radio)=> 2*radio;
+
